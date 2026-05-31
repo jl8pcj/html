9b362da102cff97748fc48c176eba91d1d2001b6
jl8pcj <jl8pcj@gmail.com>
2026-05-31 13:56:20 +0900

    1356

--- game.js
+++ game.js
@@ -353,7 +353,7 @@ function activateTwelveSkill(){
     const shot1 =
     setInterval(()=>{
 
-        x += 8;
+        x += 12;
 
         twelveShot.style.left =
             x + 'px';
@@ -411,8 +411,8 @@ function activateTwelveSkill(){
             const shot2 =
             setInterval(()=>{
 
-                x2 += 6;
-                y2 += 6;
+                x2 += 12;
+                y2 += 12;
 
                 twelveShot.style.left =
                     x2 + 'px';
